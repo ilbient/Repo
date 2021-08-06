65c848b7e5936d98f35da0541ba38ef1f4099a68
pdz <ilbient@gmail.com>
2021-08-06 11:36:46 +0200

    Uprava skriptu

--- runPi.sh
+++ runPi.sh
@@ -1,1 +1,2 @@
-python ~Repo/monitorPi.py -db=mydb -sn=test3
+#toto je shell subor ktory spusti bash skript, ktory zapise aktualnu rychlost do influxdb a zobrazi ju potom v Grafane
+python ~/Repo/monitorPi.py -db=mydb -sn=test3
